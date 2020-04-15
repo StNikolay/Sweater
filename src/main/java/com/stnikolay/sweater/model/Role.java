067d01c7044a59d1e6b59f0d250cf2c3f0b19331
StNikolay <elvis_gonzales@mail.ru>
2020-04-15 16:06:02 +0700

    added role entity

---
+++ src/main/java/com/stnikolay/sweater/model/Role.java
@@ -0,0 +1,61 @@
+package com.stnikolay.sweater.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "t_role")
+public class Role implements GrantedAuthority {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> roles;
+
+    public Role() {
+    }
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<User> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
