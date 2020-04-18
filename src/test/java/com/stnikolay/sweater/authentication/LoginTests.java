95f5f6701f6696770ec64c2a6a23eb886d8ad005
StNikolay <elvis_gonzales@mail.ru>
2020-04-18 18:11:13 +0700

    added test separation

--- src/test/java/com/stnikolay/sweater/authentication/LoginTests.java
+++ src/test/java/com/stnikolay/sweater/authentication/LoginTests.java
@@ -1,21 +1,20 @@
-package com.stnikolay.sweater;
+package com.stnikolay.sweater.authentication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class SweaterApplicationTests {
+class LoginTests {
 
     @Autowired
     private MockMvc mockMvc;
@@ -43,32 +42,6 @@ class SweaterApplicationTests {
                 .andDo(print())
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/login?error"));
-    }
-
-    @Test
-    @WithMockUser
-    public void HaveAccessToStartPage() throws Exception {
-        this.mockMvc.perform(get("/"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("There will be start page")));
-    }
-
-    @Test
-    @WithMockUser
-    public void accessDeniedToListOfUsers() throws Exception {
-        this.mockMvc.perform(get("/users"))
-                .andDo(print())
-                .andExpect(status().isForbidden());
-    }
-
-    @Test
-    @WithMockUser(roles = "ADMIN")
-    public void HaveAccessToListOfUsersFromAdmin() throws Exception {
-        this.mockMvc.perform(get("/users"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("List of users")));
     }
 
 }
