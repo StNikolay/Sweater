b4d868048903e31c7bad9a219028bfe43f28a097
StNikolay <elvis_gonzales@mail.ru>
2020-05-15 22:59:04 +0700

    update access tests

--- src/test/java/com/stnikolay/sweater/authentication/AccessTests.java
+++ src/test/java/com/stnikolay/sweater/authentication/AccessTests.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -15,13 +16,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(value = "/test-application.properties")
 class AccessTests {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser
+    @WithUserDetails(value = "ExistUser")
     public void haveAccessToStartPage() throws Exception {
         this.mockMvc.perform(get("/"))
                 .andDo(print())
@@ -30,7 +32,7 @@ class AccessTests {
     }
 
     @Test
-    @WithMockUser
+    @WithUserDetails(value = "ExistUser")
     public void accessDeniedToListOfUsers() throws Exception {
         this.mockMvc.perform(get("/users"))
                 .andDo(print())
@@ -38,7 +40,7 @@ class AccessTests {
     }
 
     @Test
-    @WithMockUser(roles = "ADMIN")
+    @WithUserDetails(value = "admin")
     public void haveAccessToListOfUsersFromAdmin() throws Exception {
         this.mockMvc.perform(get("/users"))
                 .andDo(print())
