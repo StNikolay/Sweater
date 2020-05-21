00737231e6a00ed79c23e42e2b66c7f2e34e3060
StNikolay <elvis_gonzales@mail.ru>
2020-05-21 17:08:12 +0700

    update repositories

--- src/main/java/com/stnikolay/sweater/repository/UserRepository.java
+++ src/main/java/com/stnikolay/sweater/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.stnikolay.sweater.repository;
 
 import com.stnikolay.sweater.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
