07d2c3c64a47df1c737071ca6f6aa23bb2abfe9c
StNikolay <elvis_gonzales@mail.ru>
2020-08-21 15:29:32 +0700

    added send message and check for existence methods in chat service

--- src/main/java/com/stnikolay/sweater/service/ChatService.java
+++ src/main/java/com/stnikolay/sweater/service/ChatService.java
@@ -1,8 +1,11 @@
 package com.stnikolay.sweater.service;
 
+import com.stnikolay.sweater.model.Message;
 import com.stnikolay.sweater.model.User;
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
@@ -18,8 +21,8 @@ public class ChatService {
     @Transactional
     public void registrationUserInChat(User user) throws NotFoundException {
 
-         if(restTemplate.postForObject(
-                 "http://localhost:8081/user",
+         if (restTemplate.postForObject(
+                 "http://localhost:8081/users",
                  user,
                  User.class
          ) == null)
@@ -28,4 +31,43 @@ public class ChatService {
          userService.setChatEnabledForUser(user.getUsername());
     }
 
+    public Message sendMessage(Message message) throws NotFoundException {
+
+        Message messageInfo = restTemplate.postForObject(
+                "http://localhost:8081/messages",
+                message,
+                Message.class
+        );
+
+        if (messageInfo == null)
+            throw new NotFoundException("Message cannot be sent");
+
+        return messageInfo;
+    }
+
+    public boolean checkForExistence(String username) throws NotFoundException {
+
+        try {
+            User user = userService.getUserByUsername(username);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            ResponseEntity<Boolean> response = restTemplate.exchange(
+                    "http://localhost:8081/users/check?username={username}&password={password}",
+                    HttpMethod.GET,
+                    new HttpEntity<>(headers),
+                    Boolean.class,
+                    user.getUsername(),
+                    user.getPassword()
+            );
+
+            if (response.getStatusCode() == HttpStatus.OK)
+                return response.getBody();
+            else
+                throw new NotFoundException("Request Failed with status code: " + response.getStatusCode());
+
+        } catch (UsernameNotFoundException e) {
+            return false;
+        }
+    }
 }
