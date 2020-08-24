de51aaf330ad2a87f9accb20a2c5f57f692371d5
StNikolay <elvis_gonzales@mail.ru>
2020-08-24 21:53:30 +0700

    added dialogue model

---
+++ src/main/java/com/stnikolay/sweater/model/Dialogue.java
@@ -0,0 +1,16 @@
+package com.stnikolay.sweater.model;
+
+import java.util.List;
+
+public class Dialogue {
+
+    private List<Message> messages;
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+}
